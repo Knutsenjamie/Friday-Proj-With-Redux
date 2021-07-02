9f341a35ab4f0b998243005ef64197d43471b888
“Jamie <knutsenjamie@yahoo.com>
2021-07-02 09:31:10 -0700

    populate App.js and EditKegForm.js

---
+++ src/components/EditKegForm.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReusableForm from "./ReusableForm";
+import PropTypes from "prop-types";
+
+function EditKegForm(props){
+    const { keg } = props;
+  
+    function handleEditKegFormSubmission(event) {
+      event.preventDefault();
+      props.onEditKeg({names: event.target.names.value, price: event.target.price.value, brand: event.target.brand.value, flavor: event.target.flavor.value, id: keg.id});
+    }
+  
+    return (
+      <React.Fragment>
+        <ReusableForm 
+          formSubmissionHandler={handleEditKegFormSubmission}
+          buttonText="Update Keg Information" />
+      </React.Fragment>
+    );
+  }
+
+export default EditKegForm;
