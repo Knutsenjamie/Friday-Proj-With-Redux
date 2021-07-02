26e5000ca700abc6f12d2a1bf6be3df55c99a64a
“Jamie <knutsenjamie@yahoo.com>
2021-07-02 10:34:21 -0700

    populate ReusableForm.js

---
+++ src/components/ReusableForm.js
@@ -0,0 +1,45 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function NewKegForm(props) {
+  return (
+    <React.Fragment>
+      <form onSubmit={props.formSubmissionHandler}>
+        <input
+          type='text'
+          name='name'
+          placeholder='Keg name' />
+        <hr />
+        <input
+          type='text'
+          name='brand'
+          placeholder='Keg brand' />
+        <hr />
+        <input
+          type='text'
+          name='flavor'
+          placeholder='Keg flavor' />
+        <hr />
+        <input
+          type='text'
+          name='price'
+          placeholder='Keg price' />
+        <hr />
+        <input
+          type='number'
+          name='amount'
+          placeholder='Keg amount' />
+        <hr />
+        <button type='submit'>{props.buttonText}</button>
+        <hr />
+      </form>
+    </React.Fragment>
+  );
+};
+
+ReusableForm.protoTypes = {
+  formSubmissionHandler: PropTypes.func,
+  buttonText: PropTypes.string
+};
+
+export default ReusableForm;
