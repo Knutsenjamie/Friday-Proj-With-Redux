c96cfab1624507fb6828ecd338799078fabd1aac
“Jamie <knutsenjamie@yahoo.com>
2021-07-09 10:16:10 -0700

    attempt to refactor tests to get second object as expected value w/o having to comment out- didn't work still

--- src/_tests_/reducers/keg-list-reducer.test.js
+++ src/_tests_/reducers/keg-list-reducer.test.js
@@ -4,6 +4,21 @@ describe('kegListReducer', () => {
     
     let action;
 
+    const currentState = {
+        // 1: {name: 'Test Flavor Name',
+        // brand: 'Test Brand',
+        // flavor: 'Test Flavor',
+        // price: '$11.50',
+        // pintsRemaining: 124,
+        // id: 1 },
+        2: {name: 'Test Flavor Name 2',
+        brand: 'Test Brand 2',
+        flavor: 'Test Flavor 2',
+        price: '$5.75',
+        pintsRemaining: 62,
+        id: 2 }
+}
+
     const kegData = {
         name: 'Test Flavor Name',
         brand: 'Test Brand',
@@ -11,29 +26,12 @@ describe('kegListReducer', () => {
         price: '$11.50',
         pintsRemaining: 124,
         id: 1
-    }
+    };
 
-    const currentState = {
-        // 1: {
-        // name: 'Test Flavor Name',
-        // brand: 'Test Brand',
-        // flavor: 'Test Flavor',
-        // price: '$11.50',
-        // pintsRemaining: 124,
-        // id: 1 
-        // },
+    test('Should return default state if no action type is recognized', () => {
+    expect(kegListReducer({}, { type: null })).toEqual({});
+    });
 
-        2: {
-        name: 'Test Flavor Name 2',
-        brand: 'Test Brand 2',
-        flavor: 'Test Flavor 2',
-        price: '$5.75',
-        pintsRemaining: 62,
-        id: 2 
-        },
-    }
-
-    
     test('Should successfully add new keg data to masterKegList', () => {
         const { name, brand, flavor, price, pintsRemaining, id } = kegData;
         action = {
@@ -45,7 +43,6 @@ describe('kegListReducer', () => {
             pintsRemaining: pintsRemaining,
             id: id
         };
-        
         expect(kegListReducer({}, action)).toEqual({
             [id] : {
                 name: name,
@@ -73,10 +70,6 @@ describe('kegListReducer', () => {
         });
     });
     
-        
-        test('Should return default state if there is no action type passed into the reducer', () => {
-        expect(kegListReducer({}, { type: null })).toEqual({});
-    });
 });
 
 
